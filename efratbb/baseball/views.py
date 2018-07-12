e278fe1f37ce1606717b4a9251b80af1c68175e7
Micah Gordon <micahgordon66@gmail.com>
2018-07-12 10:29:55 +0300

    Show all 3 divisions on schedule pages

--- efratbb/baseball/views.py
+++ efratbb/baseball/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import D1_Game
+from .models import D1_Game, D2_Game, D3_Game
 
 
 # Create your views here.
@@ -7,8 +7,15 @@ def index(request):
     return render(request, 'efratbb/index.html')
 
 def schedule(request):
-    games = D1_Game.objects.all()
-    return render(request, 'efratbb/schedule.html', { 'games': games })
+    d1_games = D1_Game.objects.all()
+    d2_games = D2_Game.objects.all()
+    d3_games = D3_Game.objects.all()
+    context = {
+        'd1_games': d1_games,
+        'd2_games': d2_games,
+        'd3_games': d3_games
+    }
+    return render(request, 'efratbb/schedule.html', context)
 
 def standings(request):
     return render(request, 'efratbb/standings.html')
